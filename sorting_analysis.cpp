a40940d0537406c4e8a6cbd50a512c67bb3dc17f
Amadeus Nowe <amadeusnowe34@gmail.com>
2026-05-02 10:46:12 +0800

    added randomizer
    deleted the fixed array
    added two new libraries
    added random number generator

--- sorting_analysis.cpp
+++ sorting_analysis.cpp
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-   int x[]={10,7,4,3,5,6,2,8,9,1},
-   n = 10, i,
+   int x[10],
+   n = 10, i, z,
    temp=0,count, compare=0, swap=0, assignment=0, step=0;
    
+//Number Randomizer
+   srand(time(NULL));
+   printf("Initial Array: ");
+   for( z = 0; z < n; z++){
+    
+    x[z] = (rand() % 100) + 1;
+
+    printf(" %d", x[z]);
+   }
+
+//Sorting
    for(i=0;i<n-1;i++){
       compare++;
        for (count=0; count<n-i-1;count++){
@@ -27,6 +40,7 @@ int main() {
                 for (int i=0; i<n; i++){
                 printf(" %d", x[i]); 
                 }
+                printf("\n");
                
            } else {
                 step++;
@@ -36,6 +50,7 @@ int main() {
                 for (int i=0; i<n; i++){
                 printf(" %d", x[i]); 
                 }
+                printf("\n");
 
 
            }
